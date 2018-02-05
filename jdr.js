ef7a0ed6e3f3a0d0fe747197123023a314ef79dc
vignouds <sebastien.vignoud@gmail.com>
2018-02-05 20:28:05 +0100

    Création style.css, changé noms du héro et du boss, ajouté balises strong pour leurs noms dans jdr.html

--- jdr.js
+++ jdr.js
@@ -9,11 +9,11 @@ var Boss = {
 };
 
 var hero1 = Object.create(Hero);
-hero1.name = "Toto";
+hero1.name = "Aventurier";
 hero1.strenght = 1;
 
 var boss1 = Object.create(Boss);
-boss1.name = "El Terrible Zorg";
+boss1.name = "Un Lapin Adulte";
 boss1.life = 10;
 
 
