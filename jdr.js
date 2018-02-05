fe135553d42268e9c889f19a7f2dcc2b3fff0a96
vignouds <sebastien.vignoud@gmail.com>
2018-02-05 20:58:28 +0100

    L'ennemi possède 1 force et nous frappe quand on l'attaque. Modif effectuées en conséquence.

--- jdr.js
+++ jdr.js
@@ -1,20 +1,24 @@
 var Hero = {
 	name: "",
+	life: 0,
 	strenght: 0,
 };
 
 var Boss = {
 	name : "",
 	life: 0,
+	strenght: 0,
 };
 
 var hero1 = Object.create(Hero);
 hero1.name = "Aventurier";
-hero1.strenght = 1;
+hero1.life = 20;
+hero1.strenght = 2;
 
 var boss1 = Object.create(Boss);
 boss1.name = "Un Lapin Adulte";
 boss1.life = 10;
+boss1.strenght = 1;
 
 
 
