ee7702a943b545e26c041ac4da6e9ec07c1ea640
vignouds <sebastien.vignoud@gmail.com>
2018-02-05 18:26:59 +0100

    Ajout de jquery, remplacement de l'interface inputbox/console par un interface 'graphique' en html

--- jdr.js
+++ jdr.js
@@ -16,18 +16,5 @@ var boss1 = Object.create(Boss);
 boss1.name = "El Terrible Zorg";
 boss1.life = 10;
 
-console.log("Bonjour " + hero1.name);
-console.log("Vous possédez " + hero1.strenght + " de force");
-console.log("Votre ennemi est " + boss1.name);
-console.log("Il possède " + boss1.life + " points de vie");
-console.log("");
-
-while (boss1.life > 0) {
-	console.log("Vous attaquez " + boss1.name + " et lui infligez " + hero1.strenght + " point(s) de dégats.");
-	boss1.life = boss1.life - hero1.strenght;
-	console.log(boss1.name + " possède maintenant " + boss1.life + " point(s) de vie.");
-};
-
-console.log(boss1.name + " est mort. Vous avez gagné !");
 
 
