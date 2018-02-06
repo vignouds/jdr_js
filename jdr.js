6363f4814b8d4e77d3a580876923e0c1fbe92484
vignouds <sebastien.vignoud@gmail.com>
2018-02-06 20:59:20 +0100

    Ajouté test.html et test.js, mis en forme CSS, factorisation duu code js, réglé bug du jeu qui peut continuer même quand boss est mort et des pv négatifs

--- jdr.js
+++ jdr.js
@@ -1,3 +1,4 @@
+//"Classe" Character
 var Character = {
 	initCharacter: function(name, life, strenght){
 		this.name = name;
@@ -5,15 +6,39 @@ var Character = {
 		this.strenght = strenght;
 	},
 	attack: function(target){
-		// Fonction attaquer à créer ici
+		if (target.life > 0) {
+			target.life = target.life - this.strenght;
+			if (target.life < 0) {
+				target.life = 0;
+			}
+		}
 	}
 };
 
-var hero1 = Object.create(Character);
+//"Sous-classe" Hero
+var Hero = Object.create(Character);
+
+Hero.fight = function(target){
+	this.attack(target);
+	heroAtkText = "Vous attaquez " + target.name + " et lui infligez " + this.strenght + " point(s) de dégats.";
+};
+
+//"Sous-classe" Boss
+var Boss = Object.create(Character);
+
+Boss.fight = function(target){
+	this.attack(target);
+	bossAtkText = this.name + " vous attaque et vous inflige " + this.strenght + " point(s) de dégats !"; 
+}
+
+// Initialisation de hero1 et boss1
+var hero1 = Object.create(Hero);
 hero1.initCharacter("Aventurier", 20, 2);
 
-var boss1 = Object.create(Character);
+var boss1 = Object.create(Boss);
 boss1.initCharacter ("Un Lapin Adulte", 10, 1);
+
+// Code Jquery : 
 
 $(function(){
 	$('#bossName').text(boss1.name);
@@ -24,36 +49,43 @@ $(function(){
 	$('#heroStrenght').text(hero1.strenght);
 
 	$('#btnAtk').click(function(){
-		if (hero1.life > boss1.strenght) 
+		if ((hero1.life <= 0) || (boss1.life <= 0))
 		{
-			if (boss1.life > hero1.strenght) 
-			{
-				boss1.life = boss1.life - hero1.strenght;
-				hero1.life = hero1.life - boss1.strenght;
-				$('#console').text("Vous attaquez " + boss1.name + " et lui infligez " + hero1.strenght + " point(s) de dégats.");
-				$('#console').append('<br>' + boss1.name + " vous attaque et vous inflige " + boss1.strenght + " point(s) de dégats !");
-				$('#bossLife').text(boss1.life);
-				$('#heroLife').text(hero1.life);
-			}
-			else
-			{
-				boss1.life = boss1.life - hero1.strenght;
-				hero1.life = hero1.life - boss1.strenght;
-				$('#bossLife').text(boss1.life);
-				$('#heroLife').text(hero1.life);
-				$('#console').text(boss1.name + " est mort. Vous avez gagné !");
-			}	
+			$('#console').text("Le jeu est terminé.");
 		}
 		else
+		{
+			if (hero1.life > boss1.strenght) 
 			{
-				boss1.life = boss1.life - hero1.strenght;
-				hero1.life = hero1.life - boss1.strenght;
-				$('#bossLife').text(boss1.life);
-				$('#heroLife').text(hero1.life);
-				$('#console').text("Vous êtes mort !")
-				}							
-			});
-		});
+				if (boss1.life > hero1.strenght) 
+				{
+					hero1.fight(boss1);
+					boss1.fight(hero1);
+					$('#console').text(heroAtkText);
+					$('#console').append('<br>' + bossAtkText);
+					$('#bossLife').text(boss1.life);
+					$('#heroLife').text(hero1.life);
+				}
+				else
+				{
+					hero1.fight(boss1);
+					boss1.fight(hero1);
+					$('#bossLife').text(boss1.life);
+					$('#heroLife').text(hero1.life);
+					$('#console').text(boss1.name + " est mort. Vous avez gagné !");
+				}	
+			}
+			else
+				{
+					hero1.fight(boss1);
+					boss1.fight(hero1);
+					$('#bossLife').text(boss1.life);
+					$('#heroLife').text(hero1.life);
+					$('#console').text("Vous êtes mort !")
+					}
+		}							
+	});
+});
 
 
 
